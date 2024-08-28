0b7658acc6f8dfc24df4782d46ef4fc29c920e87
abhijeetsourav <122fp0881@nitrkl.ac.in>
2024-08-29 03:10:48 +0530

    test 2

--- demo/predictor.py
+++ demo/predictor.py
@@ -67,7 +67,7 @@ class VisualizationDemo(object):
         image = image[:, :, ::-1]
         visualizer = Visualizer(image, self.metadata, instance_mode=self.instance_mode)
 
-        if predictions.has("pred_masks"):
+        if predictions.pred_masks:
             masks = np.asarray(predictions.pred_masks)
             masks = [GenericMask(x, visualizer.output.height, visualizer.output.width) for x in masks]
         else:
